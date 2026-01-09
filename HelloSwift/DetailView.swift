fc021bb898a2906719450655d22e4387392d9e04
Muna Lugonvu <munalugonvu@gmail.com>
2026-01-09 13:33:22 +0300

    Learn about navigation

---
+++ HelloSwift/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  HelloSwift
+//
+//  Created by Muna Lugonvu on 09/01/2026.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
